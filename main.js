6096002d2e5df20ac4542c6acfb6b641ac837b64
Sarah Donaghue <sarahmdonaghue@gmail.com>
2021-12-17 19:31:17 -0500

    computed properties

--- main.js
+++ main.js
@@ -2,29 +2,49 @@ var app = new Vue({
     el: '#app',
     data: {
       product: 'Socks',
-      image: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
-      inStock: false,
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
       details: ['80% cotton', '20% polyester', 'Gender-neutral'],
       variants: [
         {
           variantId: 2234,
           variantColor: 'green',
-          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg'
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg',
+          variantQuantity: 10
         },
         {
           variantId: 2235,
           variantColor: 'blue',
-          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg'
+          variantImage: 'https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg',
+          variantQuantity: 0
         }
       ],
-      cart: 0
+      cart: 0,
+      onSale: true
     },
     methods: {
-      addToCart() {
+      addToCart: function() {
         this.cart += 1
       },
-      updateProduct(variantImage) {
-        this.image = variantImage
+      updateProduct: function(index) {
+        this.selectedVariant = index
       }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if (this.onSale) {
+                return this.brand + ' ' + this.product + ' are on sale!'
+            }
+            return this.brand + ' ' + this.product + ' are not on sale.'
+        }
     }
   })
